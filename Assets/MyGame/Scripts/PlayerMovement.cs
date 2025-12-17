1329d47eed5b06946ada5ab775ee65baefc6bb32
fabian-wolf09 <fabs.wolf44@gmail.com>
2025-12-17 10:37:03 +0100

    Vorlage für Partner für PlayerMovementScript

--- Assets/MyGame/Scripts/PlayerMovement.cs
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -1,16 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject player;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = player.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space"))
+        {
+        
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            
+        }
         
     }
 }
