226d6ace2270bcd5a425def5828525aeaad63eb0
Nirual09 <101121294+Nirual09@users.noreply.github.com>
2026-01-07 10:12:04 +0100

    Added not so functional space button
    need to fix space button

--- Assets/MyGame/Scripts/PlayerMovement.cs
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -1,12 +1,15 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject player;
     private Rigidbody2D rb;
+
+    Scene currentScene = SceneManager.GetActiveScene();
 
     void Start()
     {
@@ -18,7 +21,15 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-        
+            if(currentScene.name == "Level")
+            {
+                SceneManager.LoadScene("VR");
+            }else
+            if(currentScene.name == "VR")
+            {
+                SceneManager.LoadScene("Level");
+            }
+
         }
         if (Input.GetKeyDown("a"))
         {
