77add1627f3de973260b6b0b02c6e12fb6738cd1
fabian-wolf09 <fabs.wolf44@gmail.com>
2026-01-22 19:10:04 +0100

    Aufrufen über Escape Taste vom Escape Menu

--- Assets/MyGame/Scripts/SceneManagment.cs
+++ Assets/MyGame/Scripts/SceneManagment.cs
@@ -1,9 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagment : MonoBehaviour
 {
     public GameObject endMenu;
+    public GameObject pauseMenu;
+    public GameObject player;
+
+    void Start()
+    {
+        endMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+    void Update()
+    {
+        PauseMenu();
+        EndMenu();
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -15,5 +30,24 @@ public class SceneManagment : MonoBehaviour
     public void EndMenu()
     {
         endMenu.SetActive(!endMenu.activeSelf);
+    }
+    public void Reset()
+    {
+        //TODO: Reset Player Stats
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void PauseMenu()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!endMenu.activeSelf)
+            {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            //TODO: Implement Pause Menu
+            }
+        }
+        
+        
+
     }
 }
